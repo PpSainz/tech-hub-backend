76d422ac7c4b629724c071620807937b3e279fe1
Yahir Leo <yahir.banuelos5763@alumnos.udg.mx>
2023-11-12 13:22:58 -0600

    deploying test

--- database/connection.go
+++ database/connection.go
@@ -9,7 +9,7 @@ var DB *gorm.DB
 
 func DBConnection() {
 	var err error
-	DB, err = gorm.Open(postgres.Open("host=localhost user=yiro password=yiro dbname=tech_hub_test port=3333"), &gorm.Config{})
+	DB, err = gorm.Open(postgres.Open("host=dpg-cl8i7olb7ptc73d9gnp0-a user=yiro password=TJbUmAK78EhUHrq9QCIZCcxclZAo9zi9 dbname=tech_hub_n8ut port=5432"), &gorm.Config{})
 
 	if err != nil {
 		panic("Error al conectar a la base de datos: " + err.Error())
